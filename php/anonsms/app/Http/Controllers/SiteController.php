d54e7fee9afa27eb1f1a0fd5b03158fab3d4819c
Peter Gorgone <peter@peltronic.com>
2019-03-09 11:09:42 -0800

    Clean up unused code/libs

--- php/anonsms/app/Http/Controllers/SiteController.php
+++ php/anonsms/app/Http/Controllers/SiteController.php
@@ -2,8 +2,6 @@
 // add comment here
 namespace App\Http\Controllers;
 
-use App\Libs\NavigationTaxonomy;
-
 class SiteController extends Controller
 {
     public function __construct()
@@ -16,8 +14,6 @@ class SiteController extends Controller
             '//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js',
             '//stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js',
             '/js/app/common/libs/siteUtils.js',
-            //'/js/app/common/libs/nlModalForm.js',
-            //'/js/vendor/github-macek/jquery.serialize-object.min.js',
          ]);
 
         $this->_assetMgr::registerJsInlines([
@@ -27,16 +23,11 @@ class SiteController extends Controller
         $this->_assetMgr::registerCssInlines([
             '//use.fontawesome.com/releases/v5.7.1/css/all.css',
             '//fonts.googleapis.com/css?family=Lato:100,300,400,700',
-            //'//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css',
             '//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
             '/css/app/common/base.css',
 
          ]);
 
-
-        $nt = new NavigationTaxonomy(); // %FIXME: pass from controller
-        \View::share('g_nt_html', $nt->render());
-        //$ntHtml = $nt->render();
 
         /*
         $this->middleware(function ($request, $next) {
