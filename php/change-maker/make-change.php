889424ab593f2cabf7fdc974b03359a93e196b65
Peter Gorgone <peter@peltronic.com>
2019-05-27 16:05:28 -0700

    Add simple api endpoint

--- php/change-maker/make-change.php
+++ php/change-maker/make-change.php
@@ -7,8 +7,10 @@ $usd = new Usdollar();
 
 $totalCost = ($_GET['total_cost'] ?? 0)*100; // convert to base unit (cents for USD)
 $amountProvided = ($_GET['amount_provided'] ?? 0)*100; // convert to base unit (cents for USD)
+$isPretty = array_key_exists('pretty',$_GET); 
 
 $results = $usd->makeChange( $totalCost, $amountProvided );
 
+// -- output --
 header('Content-Type: application/json');
-echo json_encode($results);
+echo json_encode($results, $isPretty?JSON_PRETTY_PRINT:null);
