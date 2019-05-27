2b818e46c39e217c66f3730158a52839f61afcb0
Peter Gorgone <peter@peltronic.com>
2019-05-27 16:49:49 -0700

    updated

--- php/change-maker/make-change.php
+++ php/change-maker/make-change.php
@@ -4,6 +4,8 @@ require_once('Changer.php');
 // http://www.dev-changemaker.com/make-change.php?total_cost=601&amount_provided=800&ctype=yen
 // http://www.dev-changemaker.com/make-change.php?total_cost=601&amount_provided=800&ctype=usd
 
+// http://changer.peterg-webdeveloper.com/make-change.php?total_cost=601&amount_provided=800&ctype=yen
+
 $cType = $_GET['ctype'] ?? 'usd'; // defaults to us dollars
 $totalCost = $_GET['total_cost'] ?? 0;
 $amountProvided = $_GET['amount_provided'] ?? 0;
